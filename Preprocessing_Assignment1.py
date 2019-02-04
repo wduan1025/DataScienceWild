8c6327a023868a646682548c206abc47ba0f61db
Daniel Nissani <danieln2568@gmail.com>
2019-02-04 13:03:50 -0500

    Fixed errors in logic.

--- Preprocessing_Assignment1.py
+++ Preprocessing_Assignment1.py
@@ -27,6 +27,7 @@ import numpy as np
 import sklearn as sk
 import matplotlib as plot
 from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
 
 
 # In[5]:
@@ -56,134 +57,121 @@ column_headers = np.array(list(churn_data))[1:-1]
 # In[9]:
 
 
-column_headers
+column_headers = list(column_headers)
 
 
 # In[10]:
 
 
-workable_data = churn_data.values
+column_headers_2 = np.array(column_headers)
 
 
 # In[11]:
 
 
-workable_data
+workable_data = churn_data.values
 
 
 # In[12]:
 
 
-for i in range(len(workable_data)):
-    for j in range(len(workable_data.T)):
-        if workable_data[i][j] == 'Male':
-            workable_data[i][j] = 0
-        if workable_data[i][j] == 'Female':
-            workable_data[i][j] = 1
-        if workable_data[i][j] == 'No':
-            workable_data[i][j] = 0
-        if workable_data[i][j] == 'Yes':
-            workable_data[i][j] = 1
-        if workable_data[i][j] == 'No phone service':
-            workable_data[i][j] = 2
-        if workable_data[i][j] == 'DSL':
-            workable_data[i][j] = 1
-        if workable_data[i][j] == 'Fiber optic':
-            workable_data[i][j] = 2
-        if workable_data[i][j] == 'No internet service':
-            workable_data[i][j] = 2
-        if workable_data[i][j] == 'Month-to-month':
-            workable_data[i][j] = 0
-        if workable_data[i][j] == 'One year':
-            workable_data[i][j] = 1
-        if workable_data[i][j] == 'Two year':
-            workable_data[i][j] = 2
-        if workable_data[i][j] == 'Electronic check':
-            workable_data[i][j] = 0
-        if workable_data[i][j] == 'Mailed check':
-            workable_data[i][j] = 1
-        if workable_data[i][j] == 'Bank transfer (automatic)':
-            workable_data[i][j] = 2
-        if workable_data[i][j] == 'Credit card (automatic)':
-            workable_data[i][j] = 3
+workable_data
 
 
 # In[13]:
 
 
-workable_data
+columns = [column_headers.index('gender'), column_headers.index('Partner'), column_headers.index('Dependents'), column_headers.index('PhoneService'), column_headers.index('MultipleLines'), column_headers.index('InternetService'), column_headers.index('OnlineSecurity'), column_headers.index('OnlineBackup'), column_headers.index('DeviceProtection'), column_headers.index('TechSupport'), column_headers.index('StreamingTV'), column_headers.index('StreamingMovies'), column_headers.index('Contract'), column_headers.index('PaperlessBilling'), column_headers.index('PaymentMethod')]
 
 
 # In[14]:
 
 
-churn_data_new = pd.DataFrame(workable_data)
+churn_data = pd.concat([churn_data, pd.get_dummies(churn_data[column_headers_2[columns]])], axis = 1)
 
 
 # In[15]:
 
 
-churn_data['Churn'].value_counts().plot(kind = 'bar')
+churn_data.shape
 
 
 # In[16]:
 
 
-labels = workable_data[:, -1]
+churn_data = churn_data.drop(column_headers_2[columns], axis = 1)
 
 
 # In[17]:
 
 
-labels = list(labels)
+churn_data.shape
 
 
 # In[18]:
 
 
-labels
+churn_data
 
 
 # In[19]:
 
 
-customer_id = workable_data[:, 0]
+churn = churn_data['Churn']
 
 
 # In[20]:
 
 
-customer_id
+churn = pd.DataFrame(churn)
 
 
 # In[21]:
 
 
-workable_data = workable_data[:, 1:-1]
+churn = churn.values
 
 
 # In[22]:
 
 
-workable_data
+c1 = 0
+c2 = 0
+for el in churn:
+    if el == 'Yes':
+        c1 += 1
+    if el == 'No':
+        c2 += 1
+
+
+# In[23]:
+
+
+c1
 
 
 # In[24]:
 
 
-for i in range(len(workable_data)):
-    if workable_data[i, -1] == ' ':
-        workable_data[i,-1] = 0
-    else:
-        workable_data[i] = workable_data[i].astype(np.float)
+c2
+
+
+# In[25]:
+
+
+churn_data = churn_data.drop('Churn', axis = 1)
+
+
+# In[26]:
+
+
+churn_data
 
 
 # In[27]:
 
 
-for i in range(len(workable_data)):
-    if workable_data[i, -1] == 0:
-        workable_data[i, -1] == sum(workable_data[-1].T)/len(workable_data[-1].T)
+workable_data = pd.concat([churn_data, pd.DataFrame(churn, columns = ['Churn'])], axis = 1)
 
 
 # In[28]:
@@ -195,77 +183,169 @@ workable_data
 # In[29]:
 
 
-sm = SMOTE(random_state = 28)
+new_columns = list(workable_data)
 
 
 # In[30]:
 
 
-new_data, new_labels = sm.fit_resample(workable_data, labels)
+workable_data = workable_data.values
 
 
 # In[31]:
 
 
-len(new_data)
+for i in range(len(workable_data)):
+    if workable_data[i][-1] == 'No':
+        workable_data[i][-1] = 0
+    if workable_data[i][-1] == 'Yes':
+        workable_data[i][-1] = 1
 
 
 # In[32]:
+
+
+workable_data
+
+
+# In[33]:
+
+
+churn_data_new = pd.DataFrame(workable_data, columns = new_columns)
+
+
+# In[34]:
+
+
+churn_data_new
+
+
+# In[36]:
+
+
+churn_data_new['Churn'].value_counts().plot(kind = 'bar')
+
+
+# In[37]:
+
+
+workable_data = workable_data[:, 1:-1]
+
+
+# In[38]:
+
+
+workable_data
+
+
+# In[39]:
+
+
+for i in range(len(workable_data)):
+    if workable_data[i, 3] == ' ':
+        workable_data[i,3] = 0
+    else:
+        workable_data[i] = workable_data[i].astype(np.float)
+
+
+# In[40]:
+
+
+workable_data.shape
+
+
+# In[41]:
+
+
+scaler = StandardScaler()
+
+
+# In[42]:
+
+
+scaler.fit(workable_data)
+
+
+# In[43]:
+
+
+data = scaler.transform(workable_data)
+
+
+# In[44]:
+
+
+data
+
+
+# In[45]:
+
+
+sm = SMOTE(random_state = 28)
+
+
+# In[46]:
+
+
+columns_without_churn = new_columns[1:-1]
+
+
+# In[47]:
+
+
+columns_without_churn = np.array(columns_without_churn)
+
+
+# In[48]:
+
+
+data.shape
+
+
+# In[49]:
+
+
+new_data, new_labels = sm.fit_resample(data, churn)
+
+
+# In[50]:
+
+
+len(new_data)
+
+
+# In[51]:
 
 
 from collections import Counter
 Counter(new_labels)
 
 
-# In[33]:
+# In[53]:
 
 
-new_data -= np.mean(new_data)
+printable_data = pd.DataFrame(data, columns = list(new_columns[1:-1]))
 
 
-# In[34]:
-
-
-new_data = new_data/(np.std(new_data))**2
-
-
-# In[35]:
-
-
-new_data
-
-
-# In[36]:
-
-
-printable_data = pd.DataFrame(new_data, columns = list(column_headers))
-
-
-# In[37]:
-
-
-printable_labels = pd.DataFrame(new_labels, columns = ["Churn"])
-
-
-# In[38]:
+# In[55]:
 
 
 printable_data
 
 
-# In[39]:
+# In[58]:
 
 
-printable_labels
+churn_data_new['Churn']
 
 
-# In[40]:
+# In[57]:
 
 
-final_data = pd.concat([printable_labels, printable_data], axis = 1)
+final_data = pd.concat([churn_data_new['Churn'], printable_data], axis = 1)
 
 
-# In[41]:
+# In[59]:
 
 
 final_data.to_csv("/Users/nissani/Desktop/Preprocessed.csv")
